a062e095c393fd6f4a4b031c8747272850bcac3a
sunaina rishi <sunaina.d.rishi@gmail.com>
2023-03-22 21:34:38 +0000

    Added test for checking correct argument being used for double method

--- spec/music_library_spec.rb
+++ spec/music_library_spec.rb
@@ -26,6 +26,7 @@ RSpec.describe MusicLibrary do
       expect(library.all).to eq [track_1, track_2]
     end
   end
+  # Note: here we aren't checking if the method is being called with the same arugment in spec and in the music_library script. This unit test will pass.
   context "Returning tracks for keyword search" do
     it "Returns two tracks in a list that meet search criteria" do
       library = MusicLibrary.new
@@ -36,6 +37,23 @@ RSpec.describe MusicLibrary do
       library.add(track_2)
       library.add(track_3)
       expect(library.search("The")).to eq [track_2, track_3]
+    end
+
+    # Can check the correct argument is being called (in the mai script i.e. keyword instead of "iuwhe") with this:
+    context "Returning tracks for keyword search" do
+      it "Returns two tracks in a list that meet search criteria" do
+        library = MusicLibrary.new
+        track_1 = double :track, matches?: false
+        expect(track_1).to receive(:matches?).with("The").and_return(false)
+        track_2 = double :track, matches?: true
+        expect(track_2).to receive(:matches?).with("The").and_return(true)
+        track_3 = double :track, matches?: true
+        expect(track_3).to receive(:matches?).with("The").and_return(true)
+        library.add(track_1)
+        library.add(track_2)
+        library.add(track_3)
+        expect(library.search("The")).to eq [track_2, track_3]
+      end
     end
 
     it "Returns two tracks in a list that meet search criteria" do
--- spec/track_spec.rb
+++ spec/track_spec.rb
@@ -1,9 +1,15 @@
 require "track"
 
 RSpec.describe Track do
-  it "returns true for keyword match" do
+  it "returns true for keyword match in ARTIST" do
     track = Track.new("Track1", "TrackCat")
     result = track.matches?("cat")
+    expect(result).to eq true
+  end
+
+  it "returns false for keyword match in TITLE" do
+    track = Track.new("Track1 one", "TrackCat")
+    result = track.matches?("ONE")
     expect(result).to eq true
   end
 
